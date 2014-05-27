c8d8d1c3ec2e8c80fe25f00aa950bc0a4197a0a0
Peter Henning <petreboy14@gmail.com>
2014-05-26 22:18:47 -0700

    Adding more functionality

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
+var Activities = require('./lib/activities');
+var Albums = require('./lib/albums');
 var Artists = require('./lib/artists');
+var Genres = require('./lib/genres');
+var Highlights = require('./lib/highlights');
+var Playlists = require('./lib/playlists');
+var Search = require('./lib/Search');
 
 var Beats = function (options) {
   this.key = options.key;
@@ -7,6 +13,30 @@ var Beats = function (options) {
 
 for (var func in Artists.prototype) {
   Beats.prototype[func] = Artists.prototype[func];
+}
+
+for (var func in Activities.prototype) {
+  Beats.prototype[func] = Activities.prototype[func];
+}
+
+for (var func in Albums.prototype) {
+  Beats.prototype[func] = Albums.prototype[func];
+}
+
+for (var func in Genres.prototype) {
+  Beats.prototype[func] = Genres.prototype[func];
+}
+
+for (var func in Highlights.prototype) {
+  Beats.prototype[func] = Highlights.prototype[func];
+}
+
+for (var func in Playlists.prototype) {
+  Beats.prototype[func] = Playlists.prototype[func];
+}
+
+for (var func in Search.prototype) {
+  Beats.prototype[func] = Search.prototype[func];
 }
 
 module.exports = Beats;
